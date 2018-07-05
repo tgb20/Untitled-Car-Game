ae717f2759ee23ae941f406575371eb576d74277
Jacob Bashista <tgb.bashista@gmail.com>
2018-07-05 18:11:44 -0400

    Coins and Stuff

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+
+
+    public GameObject[] items;
+
+    public int spawnTimer;
+
+	private void Start()
+	{
+        InvokeRepeating("SpawnItem", spawnTimer, spawnTimer);
+	}
+
+
+
+	void SpawnItem(){
+
+        if(transform.childCount == 0){
+
+
+            GameObject randomItem = items[Random.Range(0, items.Length)];
+
+            Instantiate(randomItem, transform.position, Quaternion.identity, transform);
+        }
+    }
+}
